9ae3df9cb48b7449065983a6193c579bd7afaf62
LoiseauB <bapt.loiseau@gmail.com>
2022-07-06 16:19:27 +0200

    Exo 1

---
+++ lib/00_hello.rb
@@ -0,0 +1,13 @@
+def say_hello (first_name)
+  
+  puts "Hello #{first_name}"
+end
+
+def ask_first_name
+  print "Name ? :"
+  first_name = gets.chomp
+  return first_name
+end
+
+first_name = ask_first_name
+say_hello (first_name)
