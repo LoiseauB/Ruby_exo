df514038697c9cee8ecd4ee6f7e735e12281d5cd
LoiseauB <bapt.loiseau@gmail.com>
2022-07-07 00:26:42 +0200

    ÇA MAAAARCHEEE

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,3 +1,4 @@
+marches = 10
 def lancé
   puts "lancé"
   dé = 1 + rand(6)
@@ -5,32 +6,61 @@ def lancé
 end
 
 def jeu(dé,marches)
+  puts dé
 
+    
   if dé == 6 or dé == 5
-    marches =- 1
+    marches = marches - 1
     puts "-1 marche"
   end
   if dé == 1
-    marches =+ 1
+    marches = marches+ 1
     puts "+1 marche"
   end
   if dé == 2 or dé==3 or dé==4
     marches = marches
     puts "rien"
   end
-  return marches
-end
-
-def perform
-  marches = 10
-  while marches>0
-  dé = lancé
-  jeu(dé,marches)
-  if marches>10
+    if marches>10
     puts "en bas"
     marches = 10
   end
+  puts marches
+  
+
+end
+
+def perform(marches)
+  while marches>0
+  dé = lancé
+  jeu(dé,marches)
+  
   end
 end
 
-perform
+#perform(marches)
+while marches>0
+puts "lancé"
+dé = 1 + rand(6)
+puts "Dé :#{dé}"
+
+if dé == 6 or dé == 5
+    marches = marches - 1
+    puts "-1 marche"
+  end
+  if dé == 1
+    marches = marches+ 1
+    puts "+1 marche"
+  end
+  if dé == 2 or dé==3 or dé==4
+    marches = marches
+    puts "rien"
+  end
+    if marches>10
+    puts "en bas"
+    marches = 10
+  end
+  puts"marche: #{marches}"
+end
+
+puts "GG"
